a19d60a8964ebb7d7dafcfdcc7b2c27bb59d6043
Dalia <74211933+Daliabn182@users.noreply.github.com>
2021-01-24 08:59:15 +0300

    Add files via upload

---
+++ cameraOCR.java
@@ -0,0 +1,14 @@
+package com.example.about_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cameraOCR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_o_c_r);
+    }
+}
